7406bd62c7bbfd63b9c62e9ad78052c01a374e47
Linus Kreitner <linus.kreitner@tum.de>
2021-07-09 17:22:15 +0200

    refactor: remove all files in cleanup

--- Fabric-Prod-Nw/Main-Docker-Compose/cleanup.sh
+++ Fabric-Prod-Nw/Main-Docker-Compose/cleanup.sh
@@ -2,4 +2,4 @@
 echo "Stopping and removing all containers..."
 docker rm -f $(docker ps -a -q)
 echo "Removing all files from /tmp/hyperledger ..."
-find /tmp/hyperledger -type f -delete
+rm -rf /tmp/hyperledger/*
